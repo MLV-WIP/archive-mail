1f804bf4fcbf2adae583d556c7b329f7897caa00
Michael VanLoon <m.vanloon@f5.com>
2025-01-29 14:38:32 -0800

    Working archive-old

--- archive-mail-old.py
+++ archive-mail-old.py
@@ -1,6 +1,6 @@
 #! /usr/local/bin/python3
 
-import sys, os, re, getpass, signal, argparse, json, time
+import sys, os, re, getpass, signal, argparse, json, time, datetime
 
 from imap_tools import MailBox, MailBoxFolderManager, MailBoxTls, MailMessage, AND, UidRange, H
 
@@ -36,26 +36,14 @@ def ctrlcHandler(sig, frame):
     sys.exit(1)
 
 
-message_group_count = { }
-
-def incrMsgGrp(name):
-    if name in message_group_count:
-        message_group_count[name] += 1
-    else:
-        message_group_count[name] = 1
-
-
 results = [ ]
 message_count = 0
-matches = 0
+message_folder_count = { }
 
 def summarize():
-    for result in results:
-        print(result)
+    for f in message_folder_count:
+        print(f"{f}: {message_folder_count[f]}")
     print(f"Messages processed: {message_count}")
-    print(f"Matches found: {matches}")
-    for f in message_group_count:
-        print(f"\t{f}: {message_group_count[f]}")
 
 
 #----------------------------------------------------------------------------
@@ -72,6 +60,7 @@ def parseArgs():
     parser.add_argument("-f", "--folder", help="Folder to scan for messages (accepts multiple)", action="append", required=True)
     parser.add_argument("-d", "--destination", help="Destination folder messages are moved to", required=True)
     parser.add_argument("-D", "--days", help="Specifies age in days for archiving old email")
+    parser.add_argument("-W", "--weeks", help="Specifies age in weeks for archiving old email")
     parser.add_argument("-M", "--months", help="Specifies age in months for archiving old email")
     parser.add_argument("-Y", "--years", help="Specifies age in months for archiving old email")
 
@@ -80,32 +69,10 @@ def parseArgs():
 
 parseArgs()
 
-mailServer = ""
-username = ""
-
 folderNames = [ ]
 
-if args.jsonconfig:
-    with open(args.jsonconfig, 'r') as file:
-        config = json.load(file)
-
-        if config:
-            if 'server' in config:
-                mailServer = config['server']['host']
-                username = config['server']['user']
-            
-            for sf in config['search_folders']:
-                folderNames.append(sf.lower())
-
-            for ag_data in config['archive_groups']:
-                ag = ArchiveGroup(ag_data)
-                archiveGroups.append(ag)
-
-if args.imapserver: # override config file
-    mailServer = args.imapserver
-
-if args.user: # override config file
-    username = args.user
+mailServer = args.imapserver
+username = args.user
 
 signal.signal(signal.SIGINT, ctrlcHandler)
 
@@ -117,39 +84,28 @@ signal.signal(signal.SIGINT, ctrlcHandler)
 
 print(f"EXPORT mail server: {mailServer}, username: {username}")
 
-query = AND(all=True)
-"""
-if args.month:
-    lastMonth = datetime.datetime.now() - datetime.timedelta(days=31)
-    query = AND(date_gte=lastMonth.date())
-elif args.week:
-    lastWeek = datetime.datetime.now() - datetime.timedelta(weeks=1)
-    query = AND(date_gte=lastWeek.date())
-else:
-    query = AND(all=True)
-"""
+oldDate = datetime.datetime.now()
+ageSpecified = False
+if args.years:
+    oldDate -= datetime.timedelta(days=(365.24 * args.years))
+if args.months:
+    oldDate -= datetime.timedelta(days=(30.45 * args.months))
+if args.weeks:
+    oldDate -= datetime.timedelta(weeks=args.weeks)
+if args.days:
+    oldDate -= datetime.timedelta(days=args.days)
+if not ageSpecified:
+    oldDate -= datetime.timedelta(days=365.24)
+    print(f"Defaulting age for archive to one year in the past: {oldDate}")
+
+query = AND(date_lt=oldDate.date())
 
 if args.folder:
     for folder in args.folder:
         folderNames.append(folder.lower())
 print(f"Scanning folders: {folderNames}")
 
-stringAddrs = [ ]
-if args.sender:
-    for addr in args.sender:
-        stringAddrs.append(addr.lower())
-    print(f"String-match addresses: {stringAddrs}")
-
-regexAddrs = [ ]
-if args.senderregex:
-    for addr in args.senderregex:
-        reAddr = re.compile(addr, re.IGNORECASE)
-        regexAddrs.append(reAddr)
-    print(f"Regex address matchers: {len(regexAddrs)}")
-
 destFolder = args.destination
-
-ag = ArchiveGroup(name="", dest=destFolder, text_match=stringAddrs, regex_match=regexAddrs)
 
 userpass = getpass.getpass()
 with MailBox(mailServer).login(username, userpass) as mailbox:
@@ -157,40 +113,18 @@ with MailBox(mailServer).login(username, userpass) as mailbox:
         mailbox.folder.set(folderName)
         stat = mailbox.folder.status()
         print(f"{folderName}: {stat}")
+        message_folder_count[folderName] = 0
 
         for msg in mailbox.fetch(query, headers_only=True):
             print(f"Msg UID: {msg.uid}, folder: {folderName}, date: {msg.date}, subj: '{msg.subject[0:19]}', len: {len(msg.text or msg.html)}, from: {msg.from_}")
-
-            if msg.from_:
-                found = False
-
-                for ag in archiveGroups:
-                    for addr in ag.text_match:
-                        if msg.from_.lower() == addr:
-                            print(f"matched {msg.from_}")
-                            found = True
-                            break
-                    if not found:
-                        for addr in ag.regex_match:
-                            m = addr.match(msg.from_)
-                            if m:
-                                print(f"matched {msg.from_} ({addr.pattern})")
-                                found = True
-                                break
-                    if found:
-                        print(f">>> Found match for {ag.name}")
-                        matches += 1
-                        incrMsgGrp(ag.name)
-
-                        res = mailbox.move(msg.uid, ag.destination_folder)
-                        print(f"\aMoved message {msg.uid}; result {res}")
-                        time.sleep(1.0)
-
-                        result = f"{msg.uid} '{folderName}' '{ag.destination_folder}' {msg.date} {msg.from_} '{msg.subject[0:19]}': {res}"
-                        results.append(result)
-
-                        break
-
             message_count += 1
+
+            res = mailbox.move(msg.uid, destination_folder=destFolder)
+            print(f"  ({message_count}) Moved message {folderName}/{msg.uid} {msg.date} {msg.from_}; result {res}")
+
+            #result = f"  {msg.uid} '{folderName}'->'{destFolder}' {msg.date} {msg.from_} '{msg.subject[0:19]}': {res}"
+            #results.append(result)
+
+            message_folder_count[folderName] += 1
 
 summarize()
